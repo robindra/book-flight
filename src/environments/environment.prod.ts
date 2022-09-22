b1e79eeb46c31ea24465e731e1c8fe4640225cb3
Robindra Singha <robindra.singha@impressico.local>
2022-09-22 10:26:26 +0530

    # Update enviroment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   FIREBASE_BASE_URL: 'https://identitytoolkit.googleapis.com/v1/',
+  FIREBASE_API_KEY: 'AIzaSyBTJofvfKTSOpd5TZvr647quZtDvjIhZXE',
   AMADEUS_BASE_URL: 'https://test.api.amadeus.com/v1/',  
   API_ID: 'HATF67AUUMtwfRxS0GFKAuzzAl0EBwza'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,9 @@
 export const environment = {
   production: false,
   FIREBASE_BASE_URL: 'https://identitytoolkit.googleapis.com/v1/',
+  FIREBASE_API_KEY: 'AIzaSyBTJofvfKTSOpd5TZvr647quZtDvjIhZXE',
   AMADEUS_BASE_URL: 'https://test.api.amadeus.com/v1/',
-  API_ID: 'HATF67AUUMtwfRxS0GFKAuzzAl0EBwza'
+  API_ID: 'HATF67AUUMtwfRxS0GFKAuzzAl0EBwza',
 };
 
 /*
