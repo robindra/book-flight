4d63c02df4bfc816bd7c6a560ce1b1aca650c3b9
Robindra Singha <robindra.singha@impressico.local>
2022-09-26 15:30:59 +0530

    # Added loader in app shell component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 import { AuthService } from './modules/login/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'book-flight';
-  constructor(private _authService: AuthService) {
-
+  constructor(private _authService: AuthService, private swUpdate: SwUpdate) {
+    if (this.swUpdate.isEnabled) {
+      /**
+       * This will hit whenever new version of ngsw.json available on the server 
+       */
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('New version of app is available. Do you want to reload the page now?')) {
+          window.location.reload();
+        }
+      });
+    }
   }
   ngOnInit() {
     this._authService.autoLogin();
